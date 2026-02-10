5b65b9bcafeb82b1c6739813b8942b2b4013b229
Viniciusslc <97847779+Viniciusslc@users.noreply.github.com>
2026-02-09 22:59:14 -0300

    fix: bind to render PORT

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,8 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
