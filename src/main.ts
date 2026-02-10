56d2ad863238a47f4cf3b6c953dcabb830089731
Viniciusslc <97847779+Viniciusslc@users.noreply.github.com>
2026-02-09 23:16:53 -0300

    fix(render): build with npx nest and start main.js

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  const port = Number(process.env.PORT) || 3000;
 
   await app.listen(port, '0.0.0.0');
 }
